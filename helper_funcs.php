8a5344b284b8134fd8749b7a5d4de35a1de34eaf
Rick Elrod <ricky@elrod.me>
2010-08-10 07:12:46 -0400

    Find, and report the URL of a given site (since MLA requires the title of articles and such.)

--- helper_funcs.php
+++ helper_funcs.php
@@ -5,3 +5,15 @@ function validate_csrf() {
     $_SESSION['err'] = 'CSRF Token did not validate. Please try again.';
     header("Location: ./global.php");
 }
+
+function is_valid_url($url) {
+    if(!filter_var($url, FILTER_VALIDATE_URL)) return false;
+    return true;
+}
+
+function fetch_url_title($url) {
+    $page = @file_get_contents($url, false, null, -1, 5000); // Hope it's not >5000 chars into the page.
+    preg_match('#<title>(.*)</title>#i', $page, $title);
+    if(count($title) == 0) return false;
+    return $title[1];
+}
--- mla.php
+++ mla.php
@@ -8,3 +8,13 @@ validate_csrf();
 <pre>
 <?php print_r($_POST); ?>
 </pre>
+
+<?php
+if(is_valid_url($_POST['website'])) {
+    $title = fetch_url_title($_POST['website']);
+    if(!$title) $title = "&lt;title&gt; tag could not be found.";
+} else {
+    $title = "&lt;INVALID url given.&gt;";
+}
+?>
+<b><?php echo $title; ?></b>
